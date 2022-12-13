61441b8eba7d8b2e70e4909ec5a69063d5925951
Koen-Kraaijveld <h.j.kraaijveld@student.vu.nl>
2022-12-14 00:44:35 +0100

    Added queries for factor multiplication tests

--- testing/tests/TestFactorMultiplication.py
+++ testing/tests/TestFactorMultiplication.py
@@ -31,24 +31,27 @@ class TestFactorMultiplicationExample3(unittest.TestCase):
     def test_ex3_multiply_factors_1(self):
         factor_1 = self.bn.get_cpt("A")
         factor_2 = self.bn.get_cpt("B")
-        multiplied = self.reasoner.factor_multiplication(factor_1, factor_2)
+        mult = self.reasoner.factor_multiplication(factor_1, factor_2).reset_index(drop=True)
 
-        p = multiplied["p"].reset_index(drop=True).round(2)
-        correct_p = pd.Series([0.04, 0.36, 0.48, 0.12], name="p").reset_index(drop=True)
-
-        self.assertTrue(p.equals(correct_p))
+        query = {"A": False, "B": False}
+        self.assertEqual(query_cpt_p(mult, query), 1 * 0.9)
 
     def test_ex3_multiply_factors_2(self):
         factor_1 = self.bn.get_cpt("D")
         factor_2 = self.bn.get_cpt("F")
-        multiplied = self.reasoner.factor_multiplication(factor_1, factor_2).reset_index(drop=True)
+        mult = self.reasoner.factor_multiplication(factor_1, factor_2).reset_index(drop=True)
 
-        p = multiplied["p"].round(2)
-        correct_p = pd.Series([0.04, 0.36, 0.48, 0.12], name="p").reset_index(drop=True)
+        query = {"B": False, "C": False, "D": False, "F": False}
+        self.assertEqual(query_cpt_p(mult, query), 1 * 0.9)
 
-        print(multiplied)
+        query = {"B": False, "C": False, "D": True, "F": False}
+        self.assertEqual(query_cpt_p(mult, query), 0 * 0.1)
 
-        self.assertTrue(p.equals(correct_p))
+        query = {"B": True, "C": False, "D": False, "F": False}
+        self.assertEqual(query_cpt_p(mult, query), 0.2 * 0.2)
+
+        query = {"B": True, "C": True, "D": True, "F": True}
+        self.assertEqual(query_cpt_p(mult, query), 0.95 * 0.8)
 
 
 class TestFactorMultiplicationExample4(unittest.TestCase):
@@ -56,3 +59,14 @@ class TestFactorMultiplicationExample4(unittest.TestCase):
         self.bn = BayesNet()
         self.bn.load_from_bifxml("../test_example_4.BIFXML")
         self.reasoner = BNReasoner(self.bn)
+
+
+def query_cpt(cpt, query_dict):
+    query = True
+    for var_name, query_val in query_dict.items():
+        query &= (cpt[var_name] == query_val)
+    return cpt[query]
+
+
+def query_cpt_p(cpt, query_dict):
+    return query_cpt(cpt, query_dict)["p"].iloc[0]
