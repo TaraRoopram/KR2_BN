95260a3fa6025f2c681404a7f7de8b97ba1b4002
Koen-Kraaijveld <h.j.kraaijveld@student.vu.nl>
2022-12-14 22:56:11 +0100

    Added tests for variable elimination

--- testing/tests/TestMarginalization.py
+++ testing/tests/TestMarginalization.py
@@ -15,65 +15,65 @@ class TestMarginalizationExample1(unittest.TestCase):
         self.bn.load_from_bifxml("../lecture_example.BIFXML")
         self.reasoner = BNReasoner(self.bn)
 
-    def test_ex1_marginalization_1(self):
+    def test_ex1_marginalization_single_var_1(self):
         factors = self.bn.get_cpt("Wet Grass?")
         x = ["Rain?"]
 
+        summed_out = self.reasoner.marginalize(factors, x)
         correct_summed_out = pd.DataFrame({
             "Sprinkler?": [False, False, True, True],
             "Wet Grass?": [False, True, False, True],
             "p": [1.2, 0.8, 0.10 + 0.05, 1.85]
         })
 
-        summed_out = self.reasoner.marginalize(factors, x)
         self.assertTrue(correct_summed_out.equals(summed_out))
 
-    def test_ex1_marginalization_2(self):
+    def test_ex1_marginalization_single_var_2(self):
         factors = self.bn.get_cpt("Wet Grass?")
         x = ["Sprinkler?"]
 
+        summed_out = self.reasoner.marginalize(factors, x)
         correct_summed_out = pd.DataFrame({
             "Rain?": [False, False, True, True],
             "Wet Grass?": [False, True, False, True],
             "p": [1.1, 0.9, 0.25, 1.75]
         })
 
-        summed_out = self.reasoner.marginalize(factors, x)
         self.assertTrue(correct_summed_out.equals(summed_out))
 
-    def test_ex1_marginalization_3(self):
+    def test_ex1_marginalization_single_var_3(self):
         factors = self.bn.get_cpt("Wet Grass?")
         x = ["Wet Grass?"]
 
+        summed_out = self.reasoner.marginalize(factors, x)
         correct_summed_out = pd.DataFrame({
             "Sprinkler?": [False, False, True, True],
             "Rain?": [False, True, False, True],
             "p": [1., 1., 1., 1.]
         })
 
-        summed_out = self.reasoner.marginalize(factors, x)
         self.assertTrue(correct_summed_out.equals(summed_out))
 
-    def test_ex1_marginalization_4(self):
+    def test_ex1_marginalization_multiple_vars(self):
         factors = self.bn.get_cpt("Wet Grass?")
         x = ["Sprinkler?", "Rain?"]
 
+        summed_out = self.reasoner.marginalize(factors, x)
         correct_summed_out = pd.DataFrame({
             "Wet Grass?": [False, True],
             "p": [1.35, 2.65]
         })
 
-        summed_out = self.reasoner.marginalize(factors, x)
         self.assertTrue(correct_summed_out.equals(summed_out))
 
-    def test_ex1_marginalization_5(self):
+    def test_ex1_marginalization_all_vars(self):
         factors = self.bn.get_cpt("Wet Grass?")
         x = ["Sprinkler?", "Rain?", "Wet Grass?"]
 
+        summed_out = self.reasoner.marginalize(factors, x)
         correct_summed_out = pd.Series({
             "p": 4.
         })
 
-        summed_out = self.reasoner.marginalize(factors, x)
-        self.assertTrue(summed_out.equals(correct_summed_out)) #--> is dit niet: self.assertTrue(correct_summed_out.equals(summed_out))
+        self.assertTrue(summed_out.equals(correct_summed_out))
 
--- testing/tests/TestVariableElimination.py
+++ testing/tests/TestVariableElimination.py
@@ -3,7 +3,6 @@ import unittest
 import pandas as pd
 from pandas.testing import assert_frame_equal
 
-
 from BNReasoner import BNReasoner
 from BayesNet import BayesNet
 import BNReasonerUtil as util
@@ -12,70 +11,73 @@ import BNReasonerUtil as util
 class TestMarginalizationExample1(unittest.TestCase):
     def setUp(self):
         self.bn = BayesNet()
-        self.bn_2 = BayesNet()
         self.bn.load_from_bifxml("../lecture_example.BIFXML")
-        self.bn2.load_from_bifxml("testing/test_example_6.BIFXML")
         self.reasoner = BNReasoner(self.bn)
-        self.reasoner_2 = BNReasoner(self.bn_2)
 
-    def test_ex1_marginalization_1(self):
-        factors = self.bn.get_cpt("Wet Grass?")
+    def test_ex1_var_elim_single_var_1(self):
+        factor_name = "Wet Grass?"
+        factors = {factor_name: self.bn.get_cpt(factor_name)}
         x = ["Rain?"]
 
-        correct_summed_out = pd.DataFrame({
+        var_elim = self.reasoner.variable_elimination(factors, x)
+        correct_var_elim = pd.DataFrame({
             "Sprinkler?": [False, False, True, True],
             "Wet Grass?": [False, True, False, True],
             "p": [1.2, 0.8, 0.10 + 0.05, 1.85]
         })
 
-        summed_out = self.reasoner.variable_elimination(factors, x)
-        self.assertTrue(correct_summed_out.equals(summed_out))
+        self.assertTrue(correct_var_elim.equals(var_elim))
 
-    def test_ex1_marginalization_2(self):
-        factors = self.bn.get_cpt("Wet Grass?")
+    def test_ex1_var_elim_single_var_2(self):
+        factor_name = "Wet Grass?"
+        factors = {factor_name: self.bn.get_cpt(factor_name)}
         x = ["Sprinkler?"]
 
-        correct_summed_out = pd.DataFrame({
+        var_elim = self.reasoner.variable_elimination(factors, x)
+        correct_var_elim = pd.DataFrame({
             "Rain?": [False, False, True, True],
             "Wet Grass?": [False, True, False, True],
             "p": [1.1, 0.9, 0.25, 1.75]
         })
 
-        summed_out = self.reasoner.variable_elimination(factors, x)
-        self.assertTrue(correct_summed_out.equals(summed_out))
+        self.assertTrue(correct_var_elim.equals(var_elim))
 
-    def test_ex1_marginalization_3(self):
-        factors = self.bn.get_cpt("Wet Grass?")
+    def test_ex1_var_elim_single_var_3(self):
+        factor_name = "Wet Grass?"
+        factors = {factor_name: self.bn.get_cpt(factor_name)}
         x = ["Wet Grass?"]
 
-        correct_summed_out = pd.DataFrame({
+        var_elim = self.reasoner.variable_elimination(factors, x)
+        correct_var_elim = pd.DataFrame({
             "Sprinkler?": [False, False, True, True],
             "Rain?": [False, True, False, True],
             "p": [1., 1., 1., 1.]
         })
 
-        summed_out = self.reasoner.variable_elimination(factors, x)
-        self.assertTrue(correct_summed_out.equals(summed_out))
+        self.assertTrue(correct_var_elim.equals(var_elim))
 
-    def test_ex1_variable_elimination_4(self):
-        x = ["A", "B"]
+    def test_ex1_var_elim_multiple_vars(self):
+        factor_name = "Wet Grass?"
+        factors = {factor_name: self.bn.get_cpt(factor_name)}
+        x = ["Sprinkler?", "Rain?"]
 
-        correct_variable_elminated = pd.DataFrame({
-            "C": [True, False],
-            "p": [0.376, 0.624]
+        var_elim = self.reasoner.variable_elimination(factors, x)
+        correct_var_elim = pd.DataFrame({
+            "Wet Grass?": [False, True],
+            "p": [1.35, 2.65]
         })
 
-        variable_elminated = self.reasoner_2.variable_elimination(x)
-        self.assertTrue(correct_variable_elminated.equals(variable_elminated))
+        self.assertTrue(correct_var_elim.equals(var_elim))
 
-    def test_ex1_variable_elimination_5(self):
-        x = ["A"]
+    def test_ex1_var_elim_all_vars(self):
+        factor_name = "Wet Grass?"
+        factors = {factor_name: self.bn.get_cpt(factor_name)}
+        x = ["Sprinkler?", "Rain?", "Wet Grass?"]
 
-        correct_variable_elminated = pd.DataFrame({
-            "B": [True, True, False, False],
-            "C": [True, False, True, False],
-            "p": [0.186, 0.434, 0.190, 0.190]
+        var_elim = self.reasoner.variable_elimination(factors, x)
+        correct_var_elim = pd.Series({
+            "p": 4.
         })
 
-        variable_elminated = self.reasoner_2.variable_elimination(x)
-        self.assertTrue(correct_variable_elminated.equals(variable_elminated))
+        self.assertTrue(var_elim.equals(correct_var_elim))
+
