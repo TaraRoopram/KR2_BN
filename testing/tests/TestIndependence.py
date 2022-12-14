5a3aa30211c52b69d7d3465bbb36bccfbbee5e0e
Koen-Kraaijveld <h.j.kraaijveld@student.vu.nl>
2022-12-15 00:24:25 +0100

    Added more tests for independence

--- testing/tests/TestIndependence.py
+++ testing/tests/TestIndependence.py
@@ -4,56 +4,103 @@ from BNReasoner import BNReasoner
 from BayesNet import BayesNet
 
 
-class TestIndependenceLecture(unittest.TestCase):
+class TestIndependenceLectureExample1(unittest.TestCase):
     def setUp(self):
         self.bn = BayesNet()
         self.bn.load_from_bifxml("../test_example_4.BIFXML")
         self.reasoner = BNReasoner(self.bn)
 
-    def test_lecture_example_indep_1(self):
+    def test_lecture_example_1_indep_1(self):
         x = ["B"]
         y = ["C"]
         z = ["S"]
 
-        indep = self.reasoner.is_d_separated(x, y, z)
+        indep = self.reasoner.is_independent(x, y, z)
         self.assertEqual(indep, True)
 
-    def test_lecture_example_indep_2(self):
+    def test_lecture_example_1_indep_2(self):
         x = ["X"]
         y = ["S"]
         z = ["C", "D"]
 
-        indep = self.reasoner.is_d_separated(x, y, z)
+        indep = self.reasoner.is_independent(x, y, z)
         self.assertEqual(indep, False)
 
-    def test_lecture_example_indep_3(self):
+    def test_lecture_example_1_indep_3(self):
         x = ["X"]
         y = ["S"]
         z = ["C"]
 
-        indep = self.reasoner.is_d_separated(x, y, z)
+        indep = self.reasoner.is_independent(x, y, z)
         self.assertEqual(indep, True)
 
-    def test_lecture_example_indep_4(self):
+    def test_lecture_example_1_indep_4(self):
         x = ["X", "S"]
         y = ["D"]
         z = ["B", "P"]
 
-        indep = self.reasoner.is_d_separated(x, y, z)
+        indep = self.reasoner.is_independent(x, y, z)
         self.assertEqual(indep, True)
 
-    def test_lecture_example_indep_5(self):
+    def test_lecture_example_1_indep_5(self):
         x = ["A", "S"]
         y = ["D", "X"]
         z = ["B", "P"]
 
-        indep = self.reasoner.is_d_separated(x, y, z)
+        indep = self.reasoner.is_independent(x, y, z)
         self.assertEqual(indep, True)
 
-    def test_lecture_example_indep_6(self):
+    def test_lecture_example_1_indep_6(self):
         x = ["T", "C"]
         y = ["B"]
         z = ["S", "X"]
 
-        indep = self.reasoner.is_d_separated(x, y, z)
+        indep = self.reasoner.is_independent(x, y, z)
+        self.assertEqual(indep, True)
+
+
+class TestIndependenceLectureExample2(unittest.TestCase):
+    def setUp(self):
+        self.bn = BayesNet()
+        self.bn.load_from_bifxml("../test_example_7.BIFXML")
+        self.reasoner = BNReasoner(self.bn)
+
+    def test_lecture_example_2_indep_1(self):
+        x = ["C"]
+        y = ["B", "E", "R"]
+        z = ["A"]
+
+        indep = self.reasoner.is_independent(x, y, z)
+        self.assertEqual(indep, True)
+
+    def test_lecture_example_2_indep_2(self):
+        x = ["R"]
+        y = ["A", "B", "C"]
+        z = ["E"]
+
+        indep = self.reasoner.is_independent(x, y, z)
+        self.assertEqual(indep, True)
+
+    def test_lecture_example_2_indep_3(self):
+        x = ["A"]
+        y = ["R"]
+        z = ["B", "E"]
+
+        indep = self.reasoner.is_independent(x, y, z)
+        self.assertEqual(indep, True)
+
+    def test_lecture_example_2_indep_4(self):
+        x = ["B"]
+        y = ["E", "R"]
+        z = []
+
+        indep = self.reasoner.is_independent(x, y, z)
+        self.assertEqual(indep, True)
+
+    def test_lecture_example_2_indep_5(self):
+        x = ["E"]
+        y = ["B"]
+        z = []
+
+        indep = self.reasoner.is_independent(x, y, z)
         self.assertEqual(indep, True)
